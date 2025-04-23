dcbd9a8feb8274f7552f5162b9b2a271ea35d4b8
youssef <youssefemail@gmail.com>
2025-04-24 00:25:17 +0200

    e2

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = 3000
 const baseUrl = '/api'
 
 app.use(express.json())
+app.get('/', (req, res, next) => {
+    return res.status(200).send("Welcome to Course Managment Website.")
+})
 app.use(`${baseUrl}/courses`, routes.courseRouter)
 
 app.use((req, res, next) => {
